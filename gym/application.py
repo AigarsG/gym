AigarsG/gym

commit a0a38a567248642b1f72e82ce23e9eb331a55c6f

Display training sessions in chronological order

gym/application.py
@@ -345,6 +345,7 @@ class InputRows:
 
 class Application(Tk):
 
+    _SESSION_NR_LABEL = "NR"
 
     def __init__(self, master=None):
 
@@ -367,8 +368,13 @@ class Application(Tk):
         ## create table
         self.table = Treeview(self.table_frame, selectmode="browse")
         self.table.grid(column=0, row=0, sticky=(N, S, E, W))
+
+        # Add record number column
         headers = list(map(str.upper, database.get_session_headers()))
-        self.table['columns'] = headers[1:]
+        # Replace internal _ID with record number
+        headers[0] = "NR"
+
+        self.table['columns'] = headers
         for i in range(len(headers)):
             colid = "#{}".format(i)
             text = "{}".format(headers[i])
@@ -411,8 +417,12 @@ class Application(Tk):
     def __populate_table(self):
         self.table.delete(*self.table.get_children())
         sessions = database.get_session()
-        for record in sessions:
-            self.table.insert('', 'end', text="{}".format(record[0]), values=record[1:])
+
+        # Sort by date added
+        sessions.sort(key=lambda record: record[1])
+
+        for i, record in enumerate(sessions):
+            self.table.insert('', 'end', text="{}".format(i), values=record[1:])
 
 
     def __get_selected_session(self):
@@ -435,7 +445,7 @@ class Application(Tk):
                                                      pady=5)
             session_frame = Frame(details_win)
             session_frame.grid(column=0, row=0, sticky=(S, W, N, E))
-            Label(session_frame, text="Session ID:").grid(column=0, row=0,
+            Label(session_frame, text="{}:".format(self._SESSION_NR_LABEL)).grid(column=0, row=0,
                                                           padx=5, pady=5)
             e = Entry(session_frame)
             e.insert(0, session["text"])
@@ -587,7 +597,7 @@ class Application(Tk):
             # session details
             sd_frame = Frame(form)
             sd_frame.grid(column=0, row=0)
-            Label(sd_frame, text="Session ID:").grid(column=0, row=0, padx=5,
+            Label(sd_frame, text="{}:".format(self._SESSION_NR_LABEL)).grid(column=0, row=0, padx=5,
                                                      pady=5)
             e = Entry(sd_frame)
             e.insert(0, session["text"])

commit 7ad92e96b4f837ac632cf22201e5e957de590186

Fix bug introduced with a0a38a567248 commit. Create session nr to session id mapping to use correct id for session records

gym/application.py
@@ -421,8 +421,15 @@ class Application(Tk):
         # Sort by date added
         sessions.sort(key=lambda record: record[1])
 
+        self.__session_nr_to_id = {}
+
         for i, record in enumerate(sessions):
-            self.table.insert('', 'end', text="{}".format(i), values=record[1:])
+            self.__session_nr_to_id["{}".format(i+1)] = record[0]
+            self.table.insert('', 'end', text="{}".format(i+1), values=record[1:])
+
+
+    def __get_session_id_by_nr(self, nr):
+        return self.__session_nr_to_id[nr]
 
 
     def __get_selected_session(self):
@@ -432,8 +439,8 @@ class Application(Tk):
     def __display_session_details(self):
         session = self.__get_selected_session()
         if session["text"]:
-            session_details = database.get_session_details(
-                session_id=session["text"])
+            session_id = self.__get_session_id_by_nr(session["text"])
+            session_details = database.get_session_details(session_id=session_id)
 
             # create session details window
             details_win = Toplevel(self)
@@ -515,7 +522,7 @@ class Application(Tk):
                 # get new session details
                 new_session_date = date_entry.get()
                 if new_session_date != session["values"][0]:
-                    database.update_session(session["text"],
+                    database.update_session(self.__get_session_id_by_nr(session["text"]),
                                             timestamp=new_session_date)
 
                 changes = inputs.rows[:len(session_details)]
@@ -553,7 +560,7 @@ class Application(Tk):
                         intensity = ex[4].get()
 
                         entry = {
-                            "session_id": session["text"],
+                            "session_id": self.__get_session_id_by_nr(session["text"]),
                             "exercise_id": eid,
                             "weight_kg": float(weight),
                             "reps_total": int(tot_reps),
@@ -630,7 +637,7 @@ class Application(Tk):
             ):
                 inputs.add_column(header, input_class)
             session_details = database.get_session_details(
-                session_id=session["text"])
+                session_id=self.__get_session_id_by_nr(session["text"]))
             for sd in session_details:
                 [exercise] = [e for e in exercises if e[0] == sd[2]]
                 exercise_label = exercise[1] + ", " + exercise[2]
@@ -676,7 +683,7 @@ class Application(Tk):
         session = self.__get_selected_session()
         if session["text"]:
             def do_on_yes():
-                database.delete_session(session["text"])
+                database.delete_session(self.__get_session_id_by_nr(session["text"]))
                 self.__populate_table()
                 c.destroy()
 